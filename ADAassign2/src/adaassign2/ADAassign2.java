0b8471d0831f7a00d92e4da149d6cdbf58d279a3
Wilson Liu <will.25@live.com.au>
2019-09-12 12:08:39 +1200

    initial code from Andrew, also imported everything

--- ADAassign2/src/adaassign2/ADAassign2.java
+++ ADAassign2/src/adaassign2/ADAassign2.java
@@ -9,14 +9,52 @@ package adaassign2;
  *
  * @author will2
  */
-public class ADAassign2 {
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class ADAassign2 {
+    private static final String DRIVER="com.mysql.jdbc.Driver";
+    private static final String DB_URL="jdbc:mysql://raptor2:3306/terrains";
+
     public static void main(String[] args) {
-        // TODO code application logic here
-        //testing git 
+        // obtain user name and password from keyboard
+        Scanner keyboardScanner = new Scanner(System.in);
+        System.out.print("Please enter user name:");
+        
+        String userName = keyboardScanner.nextLine();
+        System.out.print("Please enter password:");
+        
+        String password = keyboardScanner.nextLine();
+        
+        try
+        {  
+           Class.forName(DRIVER); // load the database driver for MySQL
+           System.out.println("Trying to open connection to raptor2");
+           Connection con = DriverManager.getConnection(DB_URL, userName, password);
+           Statement stmt = con.createStatement();
+           // obtain result set holding names of current databases
+           System.out.println("Executing SQL statement");
+           String command = "..."; // SQL command
+           ResultSet rs = stmt.executeQuery(command);
+           while (rs.next())
+           {  // database name string is in first column of result set
+              System.out.println(rs.getString(1));
+           }
+           System.out.println("Closing connection to raptor2");
+           stmt.close();
+           con.close();
+        }
+        catch (SQLException e)
+        {  System.out.println("SQL Exception:" + e);
+        }
+        catch (ClassNotFoundException e)
+        {  System.out.println("ClassNotFoundException:" + e);
+        }
+
     }
     
 }
